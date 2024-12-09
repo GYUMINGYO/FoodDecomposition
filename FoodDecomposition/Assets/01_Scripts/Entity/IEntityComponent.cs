95b78862464f8860cf76893c303ecef130711910
MK <minrim0615@gmail.com>
2024-12-10 00:55:53 +0900

    Clean Codes

--- FoodDecomposition/Assets/01_Scripts/Entity/IEntityComponent.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace RPG.Entities
-{
-    public interface IEntityComponent
-    {
-        public void Initialize(Entity entity);
-    }
-}
--- FoodDecomposition/Assets/01_Scripts/IPutable.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections.Generic;
-
-public interface IPutable
-{
-    List<TakableObject> GetTakableObjects();
-}
