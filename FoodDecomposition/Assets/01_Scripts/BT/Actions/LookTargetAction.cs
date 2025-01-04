9fb02f9ea44c0d8e3df217a15ae9f56e690e50ed
MK <minrim0615@gmail.com>
2025-01-04 10:24:10 +0900

    Rename speedVariable to duration

--- FoodDecomposition/Assets/01_Scripts/BT/Actions/LookTargetAction.cs
+++ FoodDecomposition/Assets/01_Scripts/BT/Actions/LookTargetAction.cs
@@ -7,12 +7,12 @@ using UnityEngine.AI;
 using Action = Unity.Behavior.Action;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "LookTarget", story: "[self] looks at [target] for [speed]", category: "Action", id: "bc23f46567b4bd64814a625ba0cb6bc7")]
+[NodeDescription(name: "LookTarget", story: "[self] looks at [target] for [duration]", category: "Action", id: "bc23f46567b4bd64814a625ba0cb6bc7")]
 public partial class LookTargetAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> Self;
     [SerializeReference] public BlackboardVariable<Transform> Target;
-    [SerializeReference] public BlackboardVariable<float> Speed;
+    [SerializeReference] public BlackboardVariable<float> Duration;
 
     Status status = Status.Running;
 
@@ -20,7 +20,7 @@ public partial class LookTargetAction : Action
     {
         Self.Value.GetComponent<NavMeshAgent>().velocity = Vector3.zero;
 
-        Self.Value.transform.DORotateQuaternion(Target.Value.rotation, Speed.Value)
+        Self.Value.transform.DORotateQuaternion(Target.Value.rotation, Duration.Value)
             .OnComplete(() => status = Status.Success);
 
         return Status.Running;
