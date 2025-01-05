4fe953e55ade13316709141657f0e25376503509
MK <minrim0615@gmail.com>
2025-01-05 19:36:41 +0900

    Rename LookTarget to LookToTarget

--- FoodDecomposition/Assets/01_Scripts/BT/Actions/LookToTarget.cs
+++ FoodDecomposition/Assets/01_Scripts/BT/Actions/LookToTarget.cs
@@ -7,20 +7,19 @@ using UnityEngine.AI;
 using Action = Unity.Behavior.Action;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "LookTarget", story: "[self] looks at [target] for [duration]", category: "Action", id: "bc23f46567b4bd64814a625ba0cb6bc7")]
+[NodeDescription(name: "LookToTarget", story: "[self] looks at [target] for [duration]", category: "Action", id: "bc23f46567b4bd64814a625ba0cb6bc7")]
 public partial class LookTargetAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> Self;
     [SerializeReference] public BlackboardVariable<Transform> Target;
     [SerializeReference] public BlackboardVariable<float> Duration;
-
     Status status = Status.Running;
 
     protected override Status OnStart()
     {
         Self.Value.GetComponent<NavMeshAgent>().velocity = Vector3.zero;
 
-        Self.Value.transform.DORotateQuaternion(Target.Value.rotation, Duration.Value)
+        Self.Value.transform.DORotateQuaternion(Target.Value.rotation, Duration.Value).SetEase(Ease.Linear)
             .OnComplete(() => status = Status.Success);
 
         return Status.Running;
