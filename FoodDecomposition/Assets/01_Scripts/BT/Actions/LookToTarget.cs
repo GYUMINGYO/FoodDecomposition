3cf1b7390c3f29d4b74e4e841d4d2c81fc984dda
MK <minrim0615@gmail.com>
2025-01-08 01:03:53 +0900

    Update LookToTarget

--- FoodDecomposition/Assets/01_Scripts/BT/Actions/LookToTarget.cs
+++ FoodDecomposition/Assets/01_Scripts/BT/Actions/LookToTarget.cs
@@ -7,13 +7,15 @@ using UnityEngine.AI;
 using Action = Unity.Behavior.Action;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "LookToTarget", story: "[self] looks at [target] for [duration]", category: "Action", id: "bc23f46567b4bd64814a625ba0cb6bc7")]
+[NodeDescription(name: "LookToTarget", story: "[self] looks at [target] for [duration] with [lookAt]", category: "Action", id: "bc23f46567b4bd64814a625ba0cb6bc7")]
 public partial class LookTargetAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> Self;
     [SerializeReference] public BlackboardVariable<Transform> Target;
     [SerializeReference] public BlackboardVariable<float> Duration;
-    Status status = Status.Running;
+    [SerializeReference] public BlackboardVariable<bool> LookAt;
+    
+    private Status status = Status.Running;
 
     protected override Status OnStart()
     {
@@ -21,7 +23,18 @@ public partial class LookTargetAction : Action
 
         Self.Value.GetComponent<NavMeshAgent>().velocity = Vector3.zero;
 
-        Self.Value.transform.DORotateQuaternion(Target.Value.rotation, Duration.Value).SetEase(Ease.Linear)
+        Quaternion targetQuaternion;
+        if (LookAt == true)
+        {
+            Vector3 dir = Target.Value.position - Self.Value.transform.position;
+            targetQuaternion = Quaternion.Euler(dir);
+        }
+        else
+        {
+            targetQuaternion = Target.Value.rotation;
+        }
+
+        Self.Value.transform.DORotateQuaternion(targetQuaternion, Duration.Value).SetEase(Ease.Linear)
             .OnComplete(() => status = Status.Success);
 
         return Status.Running;
