d2a938e6c7aeea8f98530408ec8d3d32d5cda74c
MK <minrim0615@gmail.com>
2025-01-31 09:54:12 +0900

    Implement Staff Info Generator

---
+++ FoodDecomposition/Assets/01_Scripts/Data/StaffCreator.cs
@@ -0,0 +1,23 @@
+using GM.Data;
+using GM.Staffs;
+using UnityEngine;
+
+namespace GM
+{
+    public class StaffCreator : MonoBehaviour
+    {
+        [SerializeField] private StaffInfoGeneratorSO _staffInfoGenerator;
+        [SerializeField] private StaffHandler _staffHandlerPrefab;
+
+        private void Update()
+        {
+            //! Test Code
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StaffInfo staffInfo = _staffInfoGenerator.GetRandomStaffInfo();
+                StaffHandler staffHandler = Instantiate(_staffHandlerPrefab, transform.position, Quaternion.identity);
+                staffHandler.Initialize(staffInfo);
+            }
+        }
+    }
+}
---
+++ FoodDecomposition/Assets/01_Scripts/Data/StaffInfo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace GM.Data
+{
+    public struct StaffInfo
+    {
+        public string Name;
+        public Sprite Portrait;
+    }
+}
