bea30cc279a84f989005c40fb345a715df71866a
MK <minrim0615@gmail.com>
2025-03-13 11:51:24 +0900

    Add MapEditUI

---
+++ FoodDecomposition/Assets/01_Scripts/Map/MapEditUI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace GM.Maps
+{
+    public class MapEditUI : MonoBehaviour
+    {
+        [SerializeField] private Map _map;
+        public void MapExpansion()
+        {
+            // TODO : map 특정칸에서 특정칸까지 추가하는 기능 구현
+        }
+    }
+}
